c0c1bddc4069361b3027d6f02157d013b2067f57
Frosty <nayanbastola777@gmail.com>
2023-10-29 13:54:14 -0400

    Details Complete

--- src/components/Details/DetailModal.tsx
+++ src/components/Details/DetailModal.tsx
@@ -254,6 +254,12 @@ const DetailModal: React.FC<DetailsModalProps> = ({
                           htmlFor="Credit Card"
                           data-ripple-dark="true"
                         >
+                          <input
+                            type="radio"
+                            disabled
+                            checked={formData.payment === "Credit Card"}
+                            className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-pink-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+                          />
                           <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-pink-500 opacity-0 transition-opacity peer-checked:opacity-100">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -272,7 +278,7 @@ const DetailModal: React.FC<DetailsModalProps> = ({
                         </label>
                         <label
                           className="mt-px cursor-pointer select-none  text-gray-700"
-                          htmlFor="Credit Card"
+                          htmlFor="paypal"
                         >
                           Credit Card
                         </label>
